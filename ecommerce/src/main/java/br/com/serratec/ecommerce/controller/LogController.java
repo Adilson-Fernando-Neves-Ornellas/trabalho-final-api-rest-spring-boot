c15abf4a4ea705145a5c3ae9985fb6f941f6b7af
brunazimbrao <bruna.z.silva@aluno.senai.br>
2023-10-18 16:02:32 -0300

    fix:comflito merge

--- ecommerce/src/main/java/br/com/serratec/ecommerce/controller/LogController.java
+++ ecommerce/src/main/java/br/com/serratec/ecommerce/controller/LogController.java
@@ -11,25 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.serratec.ecommerce.model.Log;
 import br.com.serratec.ecommerce.service.LogService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+
+
 
 @RestController
 @RequestMapping("/logs")
-@Api(value = "LOGS")
 public class LogController {
     
     @Autowired
     private LogService logService;
 
     @GetMapping
-    @ApiOperation(value = "RETORNA UMA LISTA COM TODOS ")
     public ResponseEntity<List<Long>> obterTodos() {
         return ResponseEntity.ok(logService.obterTodos());
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "RETORNA UM EXPECIFICO PELO ID ")
     public ResponseEntity<Log> obterPorId(@PathVariable long id) {
         return ResponseEntity.ok(logService.obterPorId(id));
     }
