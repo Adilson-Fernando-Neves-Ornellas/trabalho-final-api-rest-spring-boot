0aa8cf2bc95a66055f2685fccac827eef5d78ecf
Adilson Fernando <ornellas00@hotmail.com>
2023-10-19 16:57:00 -0300

    feat: colcando a função de enviar email de com as informações do pedido ao fazer um pedido

--- ecommerce/src/main/java/br/com/serratec/ecommerce/service/PedidoService.java
+++ ecommerce/src/main/java/br/com/serratec/ecommerce/service/PedidoService.java
@@ -113,4 +113,6 @@ public class PedidoService {
 
         return pedido;
     }
+
+
 }
--- ecommerce/src/main/java/br/com/serratec/ecommerce/service/UsuarioService.java
+++ ecommerce/src/main/java/br/com/serratec/ecommerce/service/UsuarioService.java
@@ -34,9 +34,6 @@ public class UsuarioService  {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    //@Autowired
-    //private PedidoService pedidoService;
-
     @Autowired
     private PasswordEncoder passwordEncoder;
 
